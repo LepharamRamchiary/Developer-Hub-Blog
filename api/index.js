495dc2fb67a81ea43094cdc16b6539c1f3ac119e
Lepha <lepharamchiary@gmail.com>
2024-05-31 13:07:55 +0530

    mongodeb connections

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
-const PORT = 3000;
+import mongoose from "mongoose";
+import 'dotenv/config';
+const PORT = 3011;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB connected");
+}).catch(err => {
+    console.log(err);
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
