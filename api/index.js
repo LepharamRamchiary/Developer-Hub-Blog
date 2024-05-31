52766fc7e26451d451569c909fa6587471c614d9
Lepha <lepharamchiary@gmail.com>
2024-05-31 16:31:57 +0530

    simple changes

--- api/index.js
+++ api/index.js
@@ -1,21 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
-import 'dotenv/config';
-const PORT = 3011;
-const MONGODB_URI = process.env.MONGODB_URI;
+import dotenv from 'dotenv';
+const PORT = process.env.PORT || 3011;
 
+dotenv.config();
+   
 const app = express();
 
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("DB connected");
 }).catch(err => {
     console.log(err);
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { 
     console.log(`Server is running on port: ${PORT}`)
 })
+
 
