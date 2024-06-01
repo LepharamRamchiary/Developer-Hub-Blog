ffdc0b7d2bb43f334c9b60bc75703f968c0f0e64
Lepha <lepharamchiary@gmail.com>
2024-06-01 20:45:51 +0530

    create a footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from "./pages/Projects";
 import SingIn from "./pages/SingIn";
 import SingUp from "./pages/SingUp";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
           <Route path="/sing-up" element={<SingUp />} />
           <Route />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
