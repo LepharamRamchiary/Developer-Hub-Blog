523a296757d1d890b5a23f9cdb59dbae09388df3
Lepha <lepharamchiary@gmail.com>
2024-06-01 11:35:33 +0530

    add a middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const singup = async (req, res) => {
+export const singup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "All fields are required" });
+        next(errorHandler(400, "All field are required"));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -20,6 +21,6 @@ export const singup = async (req, res) => {
         await newUser.save();
         res.status(200).json("Singup successfully");
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 const PORT = 3011;
-dotenv.config();                          
+dotenv.config();
 
-// Middelware
+// Middleware
 const app = express();
 
 // sending json data to db
@@ -18,10 +18,22 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log(err);
 })
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
 })
- 
+
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+
+// Middleware for error heading
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
