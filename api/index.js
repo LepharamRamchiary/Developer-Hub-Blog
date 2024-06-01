510b6a559c15d4e3450a7d8ea47f4088aac13ab9
Lepha <lepharamchiary@gmail.com>
2024-06-01 11:16:54 +0530

    create singup api route

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js";
-
+import authRouter from "./routes/auth.route.js";
 const PORT = 3011;
-
 dotenv.config();                          
-      
+
+// Middelware
 const app = express();
+
+// sending json data to db
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("DB connected");
@@ -21,3 +24,4 @@ app.listen(PORT, () => {
  
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
