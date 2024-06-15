9797b3b7fb1f9d7809f063ec8b41f66979de742c
Lepha <lepharamchiary@gmail.com>
2024-06-15 10:32:56 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,9 +6,12 @@ import authRouter from "./routes/auth.route.js";
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const PORT = 8000;
 dotenv.config();
+
+const __dirname = path.resolve();
 
 // Middleware
 const app = express();
@@ -33,6 +36,12 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
 app.use("/api/comment", commentRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
 
 
 // Middleware for error heading
